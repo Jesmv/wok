6aecf4458f54c807542b8c96f3e7daef5a03ad2c
Jessica Manso <jesmv83@gmail.com>
2017-02-16 21:48:10 +0100

    cambios en perfil

--- editarPerfil.php
+++ editarPerfil.php
@@ -21,17 +21,39 @@
   <?php 
 
       if (isset($_SESSION['sesionIniciada'])) {
-         
+         if(isset($_POST['modificar'])) {
+
+          if (checkName() && downloadImage() && checkFirma() && changePass()) {
+            
+            $_SESSION['usuario'] -> setName($_POST['name']);
+            $_SESSION['usuario'] -> setEmail($_POST['email']);
+            $_SESSION['usuario'] -> setFirma($_POST['firma']);
+            $_SESSION['usuario']->setAvatar($_SESSION['nuevoAvatar']);
+
+            $dataBase = conectarbd();
+            $consulta = "UPDATE usuario SET Password='".$_SESSION['usuario']->getPass()."', Nombre='".$_SESSION['usuario']->getName()."', Email='".$_SESSION['usuario']->getEmail()."', Avatar='".$_SESSION['usuario']->getAvatar()."' Where Login='".$_SESSION['usuario']->getUser()."'";
+            saveData($dataBase, $consulta); // Guardo los datos nuevos en la base de datos.
+            closebd($dataBase);
+
+            echo "<script> alert('Perfil modificado.') </script>";
+
+          }   
+          
+        } elseif (isset($_POST['final'])){
+          echo "<script>
+                window.location.pathname='../practica12y13/practica13/usuario.php';
+              </script>";
+        }
       ?>
         <nav class="nav-extended lime accent-21">
             <div class="nav-wrapper lime accent-21">
             <a href="#" class="brand-logo">WOK</a>
             <ul class="right hide-on-med-and-down">
                 <li><a href="home.php">Home</a></li>
-                <li><a href="registro.php">Editar Perfil</a></li>
+                <li><a href="#">Editar Perfil</a></li>
                 <li><a href="nuevoPedido.php">Nuevo Pedido</a></li>
                 <li><a href="misPedidos.php">Mis pedidos</a></li>
-                <li><a href="#">Contacto</a></li>
+                <li><a href="contacto.php">Contacto</a></li>
                 <li><a href="cerrar.php">Cerrar Sesión</a></li>
             </ul>
             </div>
@@ -39,6 +61,82 @@
               Bienvenid@ <?php echo $_SESSION['usuario']->getName(); ?>
             </div>
         </nav>
+        <h1 class="text-center">Editar Perfil</h1>
+        <h2 class="text-center">Datos de la Cuenta</h2>
+
+        <div class="row">
+          <form class="col s5 offset-m2" method="post" action="#" ENCTYPE="multipart/form-data">
+            <div class="row">
+            <div class="input-field col s12">
+              <input placeholder="Placeholder" id="user" type="text" value="<?php echo $_SESSION['usuario'] -> getUser(); ?> " readonly>
+              <label for="user">User</label>
+            </div>
+            </div>
+            <div class="row">
+            <div class="input-field col s12">
+              <input id="name" name="name" type="text" value="<?php echo $_SESSION['usuario'] -> getName(); ?> " required/>
+              <label for="name">Name</label>
+            </div>
+          </div>
+          <div class="row">
+            <div class="file-field input-field">
+              <div class="btn">
+                <span>File</span>
+                <input type="file" name="imagen">
+              </div>
+              <div class="file-path-wrapper">
+                <input class="file-path validate" id="file" type="text" >
+              </div>
+            </div>
+          </div>
+          <div class="row">
+            <div class="input-field col s12">
+              <input id="email" type="email" name="email" value="<?php echo $_SESSION['usuario'] -> getEmail(); ?> " required/>
+              <label for="email">Email</label>
+            </div>
+          </div>
+          <div class="row">
+            <div class="input-field col s12">
+              <input id="firma" type="text" name="firma" value="<?php echo $_SESSION['usuario'] -> getFirma(); ?> " required/>
+              <label for="firma">Firma</label>
+            </div>
+          </div>
+          <div class="row">
+            <div class="input-field col s12">
+              <input placeholder="Placeholder" id="rango" type="text" value="<?php echo $_SESSION['usuario'] -> getTipo(); ?> " readonly>
+              <label for="rango">User</label>
+            </div>
+            </div>
+          <h2 class="text-center">Cambiar Contraseña</h2>
+          <div class="row">
+            <div class="input-field col s12">
+              <input name="oldPass" id="oldPass" name="oldPass" type="password">
+              <label for="oldPass">Antigua contraseña</label>
+            </div>
+          </div>
+          <div class="row">
+            <div class="input-field col s12">
+              <input name="newPass" id="newPass" name="NewPass" type="password">
+              <label for="newPass">Nueva contraseña</label>
+            </div>
+          </div>
+          <div class="row">
+            <div class="input-field col s12">
+              <input name="repeatNewPass" id="repeatNewPass" name="repeatNewPass" type="password" >
+              <label for="repeatNewPass">Repita nueva contraseña</label>
+            </div>
+          </div>
+          <div class="row">
+              <input class="btn waves-effect waves-light" type="submit" name="modificar" id="modificar">
+          </div>
+        </form>
+
+        <div class="col s3">
+          <img src="<?php echo $_SESSION['usuario'] -> getAvatar(); ?>" alt="Avatar" class=" circle responsive-img" with="200px" height="200px">
+        </div>
+      </div>
+
+
       <?php 
       } else {
       ?>
--- funciones.php
+++ funciones.php
@@ -1,6 +1,6 @@
 <?php 
 	function conectarbd() {
-		return $bdd = new mysqli('localhost', 'root', 'guapa', 'wok');
+		return $bdd = new mysqli('localhost', 'root', '', 'wok');
 	}
 
 	function saveData($registro, $consulta){
@@ -10,5 +10,95 @@
 
 	function closebd($bdd) {
 		return $bdd -> close();
+	}
+
+	function checkName() {
+		$text = trim($_POST['name']);
+		if($text== ""){
+			echo "<script> alert('Introduzca un nombre.') </script>";
+			return false;
+		} else {
+			return true;
+		}
+	}
+
+	function checkFirma() {
+		$text = trim($_POST['firma']);
+		if($text== null){
+			echo "<script> alert('Introduzca la firma.') </script>";
+			return false;
+		} else {
+			return true;
+		}
+	}
+
+	/*Guarda la imagen en una carpeta. Primero comprueba que el campo no esté vacio. Lo hago comprobando si es el error 4, que indica que se encuentra vacio el input del file, si es así me devuelve true, ya que considero que se puede tener vacio porque no se quiera cambiar la imagen.. */
+	function downloadImage() {
+
+		if ($_FILES['imagen']['error']==4) {
+			
+			return true;
+		} else {
+
+			/*En este if else se comprueba que la imagen se ha subido. Si es correcto creamos una variable */
+			if (is_uploaded_file ($_FILES['imagen']['tmp_name'] )) {
+				$fileName = $_FILES['imagen']['name'];
+				$directoryName = "imagenes/";
+				$finalName = $directoryName.$fileName;
+
+				/*Si la imagen es mayor de 500kb nos devolverá un error y no continuará*/
+				if ($_FILES['imagen']['size'] > 500000) {
+					echo "<script> alert('La imagen supera el tamaño. Máximo 500kb.') </script>";
+					return false;
+				} 
+				
+				/*En el caso de que no tuviesemos permisos no nos dejaria subir la foto y nos mostraría un error, en el caso de que todo fuese correcto, la imagen se guardaría en la carpeta de imágenes y se guarda en una sesión llamada nuevo Avatar para despues guardar la dirección en la base de datos.*/
+				if (is_dir($directoryName)){ 
+					$timeName = time();
+					$fileName = $timeName."-".$fileName;
+					$finalName = $directoryName.$fileName;
+					move_uploaded_file ($_FILES['imagen']['tmp_name'],$finalName);
+					
+					$_SESSION['nuevoAvatar']=$finalName;
+					
+					return true;
+				} else {
+					echo "<script> alert('Error al cargar la imagen') </script>";
+					return false;
+				}
+				
+			} else {
+				echo "<script> alert('Error al cargar la imagen.') </script>";
+				return false;
+			}
+		}	
+
+	}
+
+	function changePass() {
+
+		/*Si los tres campos de la contraseña están vacios, devolverá true porque se entiende que no se quiere hacer ningún cambio, si alguno de los tres esta rellenado continuará haciendo la comprobación.*/
+		if($_POST['oldPass'] == "" && $_POST['newPass'] == "" && $_POST['repeatNewPass'] == ""){
+				return true;
+			}
+
+		/*Guardo las contraseñas encriptadas. Lo primero comprueblo que la contraseña coincide con la del usuario, en el caso de que no sea así devuelve un alert. Despues comprueba que las dos contraseñas nuevas son iguales, si lo son devuelve true, sino muestra un alert diciendo que no coinciden. */
+		$oldPass = md5($_POST['oldPass']);
+		$newPass = md5($_POST['newPass']);
+		$repeatNewPass = md5($_POST['repeatNewPass']);
+
+		if($oldPass === $_SESSION['usuario']->getPass()) {
+			if($_POST['newPass'] === $_POST['repeatNewPass']){
+				$_SESSION['usuario']->setPass(md5($_POST['repeatNewPass']));
+				return true;
+				
+			} else {
+				echo "<script> alert('Las contraseñas no coinciden.') </script>";
+				return false;
+			}
+		} else {
+			echo "<script> alert('Contraseña incorrecta.') </script>";
+			return false;
+		}
 	}
  ?>
